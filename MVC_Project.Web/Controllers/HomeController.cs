ea3f6032a391a40c033b6f46d2c1a697c07421ac
Ankit Kumar <sharmaankitkumar184@gmail.com>
2025-06-04 13:12:14 +0530

    Improved Display for Employee page

---
+++ MVC_Project.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Homepage()
+        {
+            return View();
+        }
+    }
+}
